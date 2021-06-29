72850182407a66f747d505ca2492a73d3776b836
Nicholas Raynes <nicholas.r.raynes@gmail.com>
2021-06-29 13:40:25 -0600

    added some properties to computer.js

--- src/computer.js
+++ src/computer.js
@@ -1,5 +1,12 @@
 class Computer {
-  
-}
+  constructor (name,operatingSystem,processor,memory,graphics) {
+    this.name = name;
+    this.operatingSystem = operatingSystem;
+    this.processor = processor;
+    this.memory = memory;
+    this.graphics = graphics;
+    this.on = false;
+  };
+};
 
-module.exports = Computer
+module.exports = Computer;
